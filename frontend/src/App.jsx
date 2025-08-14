091f7639e3dfbc3580b8fd0cba3ee55359c37e5e
pikach-u <ziwon05@naver.com>
2025-08-14 11:02:07 +0900

    stat card 디자인중

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
+import StatsCard from "./components/StatsCard";
 
 const App = () => {
   return (
     <div className="min-h-screen bg-gray-50">
       <div className="bg-red-100 max-w-4xl mx-auto px-4 py-8">
         <Header />
+        <StatsCard />
       </div>
     </div>
   );
