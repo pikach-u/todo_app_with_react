d7d7d733dadd7e5795560b93f2a3284ada22910e
pikach-u <ziwon05@naver.com>
2025-08-14 10:15:15 +0900

    Frontend Delete Todo test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,17 @@
+import { useEffect } from "react";
+import { todoApi } from "./services/api";
+
 const App = () => {
+  useEffect(() => {
+    const test = async () => {
+      const data = await todoApi.deleteTodo(6);
+
+      console.log(data);
+    };
+
+    test();
+  }, []);
+
   return <div className="bg-red-100">App</div>;
 };
 
